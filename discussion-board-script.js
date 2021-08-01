dc4e6d5e6344f1d5e764191ae764ea162f79b676
kunj-zluri <kunjmaniar1999@gmail.com>
2021-08-01 22:58:31 +0530

    Update discussion-board-script.js

--- discussion-board-script.js
+++ discussion-board-script.js
@@ -110,7 +110,7 @@ function jQueryCode(runnerFunc) {
 
 		add_after.after(`
       <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document" style="max-width: 90%">
+        <div class="modal-dialog modal-lg" role="document" style="width: 90%">
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="exampleModalLabel">Meteor Discussion Board</h5>
